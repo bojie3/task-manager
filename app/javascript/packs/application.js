265e4ab9ac5f0e45d3a972a3ef631416b339ee3e
bojie3 <95684564+bojie3@users.noreply.github.com>
2022-01-25 21:48:04 +0800

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//commented out turbolink as some guide said it was a quick fix for back button not working
+
 import Rails from "@rails/ujs"
 // import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
